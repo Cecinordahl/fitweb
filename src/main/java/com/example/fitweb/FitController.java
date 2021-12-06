0e9c8172c1e608e95fb3d062bb7aa37fb6ac7fee
cecinordahl <cecilie.nordahl@gmail.com>
2021-12-06 18:21:35 +0100

    calculator finished

--- src/main/java/com/example/fitweb/FitController.java
+++ src/main/java/com/example/fitweb/FitController.java
@@ -1,13 +1,26 @@
 package com.example.fitweb;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FitController {
+    public Integer ageInput;
 
     @GetMapping("/")
-    public String homePage(){
-        return "index";
+    public String homePage(Model model){
+        if(ageInput != null){
+            model.addAttribute("inputAge", ageInput);
+        }
+        return "macro_calculator";
+    }
+
+    @PostMapping("/")
+    public String homePagePost(@RequestParam Integer age){
+        ageInput = age;
+        return "redirect:/";
     }
 }
